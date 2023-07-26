95648f9e344452a5be5762a7cb4d6b77cf485788
Tafseer Khan <tafseer.k.khan@gmail.com>
2023-07-26 11:07:28 -0500

    add more imports to import helper

--- utils/dreamland/import.go
+++ utils/dreamland/import.go
@@ -1,11 +1,13 @@
 package dreamland
 
 import (
+	_ "github.com/taubyte/odo/clients/p2p/auth"
 	_ "github.com/taubyte/odo/clients/p2p/hoarder"
 	_ "github.com/taubyte/odo/clients/p2p/monkey"
 	_ "github.com/taubyte/odo/clients/p2p/patrick"
 	_ "github.com/taubyte/odo/clients/p2p/seer"
 	_ "github.com/taubyte/odo/clients/p2p/tns"
+	_ "github.com/taubyte/odo/protocols/auth"
 	_ "github.com/taubyte/odo/protocols/hoarder"
 	_ "github.com/taubyte/odo/protocols/monkey"
 	_ "github.com/taubyte/odo/protocols/patrick"
