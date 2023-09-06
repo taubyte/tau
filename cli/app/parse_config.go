fca60427ed15f8f109103bab97a94a0365a9246f
Malobika Roy <malobikaroy0001@gmail.com>
2023-09-05 21:36:47 -0400

    Update parse_config.go
    Added comments

--- cli/app/parse_config.go
+++ cli/app/parse_config.go
@@ -21,80 +21,114 @@ import (
 
 // TODO: move to config as a methods
 
-// Parse from yaml
+/* The function takes a context and a shape as input, then processes the configuration data stored in a YAML file.
+This function constructs the necessary file paths based on a root directory and the shape parameter.
+It reads and interprets the YAML data into a structure named src. Additionally, it performs adjustments to certain file paths and keys within the configuration.*/
 func parseSourceConfig(ctx *cli.Context, shape string) (string, *config.Node, *config.Source, error) {
+	
+    	// Retrieve the 'root' path from the command line context or use the default if not specified.
 	root := ctx.Path("root")
 	if root == "" {
-		root = config.DefaultRoot
-	}
-
+	        root = config.DefaultRoot
+	    }
+	
+	// Check if 'root' is an absolute path; return an error if it's not.
 	if !filepath.IsAbs(root) {
-		return "", nil, nil, fmt.Errorf("root folder `%s` is not absolute", root)
-	}
-
+	        return "", nil, nil, fmt.Errorf("root folder `%s` is not absolute", root)
+	    }
+	
+	// Define the path to the 'configRoot' folder by combining 'root' with "/config".
 	configRoot := root + "/config"
+	
+	// Generate the configuration file path ('configPath') based on 'shape' and 'configRoot'.
 	configPath := ctx.Path("path")
 	if configPath == "" {
-		configPath = path.Join(configRoot, shape+".yaml")
-	}
+	        configPath = path.Join(configRoot, shape+".yaml")
+	    }
 
+	// Read the content of the configuration file specified by 'configPath'.
 	data, err := os.ReadFile(configPath)
 	if err != nil {
-		return "", nil, nil, fmt.Errorf("reading config file path `%s` failed with: %w", configPath, err)
-	}
+	        return "", nil, nil, fmt.Errorf("reading config file path `%s` failed with: %w", configPath, err)
+	    }
+	
+	// ... Additional processing and parsing of 'data' ...
+	
+	// Return the results, including the constructed 'configPath' and any errors.
+	// Placeholder, actual return values depend on further processing.
 
-	src := &config.Source{}
 
+	// Create an empty 'config.Source' struct to hold the parsed YAML data.
+	src := &config.Source{} 
+	
+	// Attempt to unmarshal the 'data' (YAML content) into the 'src' struct.
 	if err = yaml.Unmarshal(data, &src); err != nil {
-		return "", nil, nil, fmt.Errorf("yaml unmarshal failed with: %w", err)
+	    return "", nil, nil, fmt.Errorf("yaml unmarshal failed with: %w", err)
 	}
-
+	
+	// Update the paths for private and public keys, and the 'Swarmkey' to use the 'configRoot' folder.
+	// This ensures that these file paths are constructed relative to the 'configRoot' directory.
 	src.Domains.Key.Private = path.Join(configRoot, src.Domains.Key.Private)
+	
+	// Check if a public key file path is provided in the configuration.
 	if src.Domains.Key.Public != "" {
-		src.Domains.Key.Public = path.Join(configRoot, src.Domains.Key.Public)
+	    // If a public key path is provided, update it to use the 'configRoot' directory.
+	    src.Domains.Key.Public = path.Join(configRoot, src.Domains.Key.Public)
 	}
+	
+	// Update the 'Swarmkey' path to use the 'configRoot' directory.
 	src.Swarmkey = path.Join(configRoot, src.Swarmkey)
 
+	// Validate keys based on the provided protocols and key data.
 	err = validateKeys(src.Protocols, src.Domains.Key.Private, src.Domains.Key.Public)
 	if err != nil {
-		return "", nil, nil, err
+	    return "", nil, nil, err
 	}
-
+	
+	// Create a 'config.Node' struct named 'protocol' with specific field values.
 	protocol := &config.Node{
-		Root:            root,
-		Shape:           shape,
-		P2PAnnounce:     src.P2PAnnounce,
-		P2PListen:       src.P2PListen,
-		Ports:           src.Ports.ToMap(),
-		Location:        src.Location,
-		NetworkFqdn:     src.NetworkFqdn,
-		GeneratedDomain: src.Domains.Generated,
-		ServicesDomain:  convertToServiceRegex(src.NetworkFqdn),
-		HttpListen:      "0.0.0.0:443",
-		Protocols:       src.Protocols,
-		Plugins:         src.Plugins,
-		Peers:           src.Peers,
-		DevMode:         ctx.Bool("dev-mode"),
+	    Root:            root,                               // Set the root directory.
+	    Shape:           shape,                              // Set the shape.
+	    P2PAnnounce:     src.P2PAnnounce,                    // Set P2P announce configuration.
+	    P2PListen:       src.P2PListen,                      // Set P2P listen configuration.
+	    Ports:           src.Ports.ToMap(),                  // Convert Ports to a map.
+	    Location:        src.Location,                       // Set location configuration.
+	    NetworkFqdn:     src.NetworkFqdn,                    // Set Network Fully Qualified Domain Name.
+	    GeneratedDomain: src.Domains.Generated,              // Set the generated domain.
+	    ServicesDomain:  convertToServiceRegex(src.NetworkFqdn), // Convert NetworkFqdn to service regex.
+	    HttpListen:      "0.0.0.0:443",                      // Set HTTP listen address and port.
+	    Protocols:       src.Protocols,                      // Set the protocols.
+	    Plugins:         src.Plugins,                        // Set the plugins.
+	    Peers:           src.Peers,                          // Set the peers.
+	    DevMode:         ctx.Bool("dev-mode"),               // Set the development mode flag.
 	}
-
+	
+	// Check if the 'src.Privatekey' is empty. If it is, return an error.
 	if len(src.Privatekey) == 0 {
-		return "", nil, nil, errors.New("private key can not be empty")
+	    return "", nil, nil, errors.New("private key cannot be empty")
 	}
-
+	
+	// Decode the 'src.Privatekey' from base64 encoding.
 	base64Key, err := base64.StdEncoding.DecodeString(src.Privatekey)
 	if err != nil {
-		return "", nil, nil, fmt.Errorf("converting private key to base 64 failed with: %s", err)
+	    return "", nil, nil, fmt.Errorf("converting private key to base 64 failed with: %s", err)
 	}
-
+	
+	// Set the 'protocol.PrivateKey' field with the decoded private key.
 	protocol.PrivateKey = []byte(base64Key)
-
+	
+	// Attempt to parse the 'Swarmkey' from the 'src.Swarmkey' value.
+	// If there's an error during parsing, return the error.
 	if protocol.SwarmKey, err = parseSwarmKey(src.Swarmkey); err != nil {
-		return "", nil, nil, err
+	    return "", nil, nil, err
+	}
+	
+	// Attempt to parse domain validation keys (private and public keys) from 'src.Domains.Key'.
+	// If there's an error during parsing, return the error.
+	if protocol.DomainValidation, err = parseValidationKey(&src.Domains.Key); err != nil {
+	    return "", nil, nil, err
 	}
 
-	if protocol.DomainValidation, err = parseValidationKey(&src.Domains.Key); err != nil {
-		return "", nil, nil, err
-	}
 
 	pkey, err := crypto.UnmarshalPrivateKey(protocol.PrivateKey)
 	if err != nil {
@@ -109,19 +143,29 @@ func parseSourceConfig(ctx *cli.Context, shape string) (string, *config.Node, *c
 	return pid.Pretty(), protocol, src, nil
 }
 
+// Define a function named 'parseSwarmKey' that takes a 'filepath' as a parameter.
 func parseSwarmKey(filepath string) (pnet.PSK, error) {
-	if len(filepath) > 0 {
-		data, err := os.ReadFile(filepath)
-		if err != nil {
-			return nil, fmt.Errorf("reading %s failed with: %w", filepath, err)
-		}
+    // Check if the 'filepath' is not empty.
+    if len(filepath) > 0 {
+        // Read the content of the file specified by 'filepath'.
+        data, err := os.ReadFile(filepath)
+        if err != nil {
+            // If there's an error during file reading, return nil for the PSK and an error with a description.
+            return nil, fmt.Errorf("reading %s failed with: %w", filepath, err)
+        }
 
-		return formatSwarmKey(string(data))
-	}
+        // Call the 'formatSwarmKey' function with the read 'data' to parse and format it as a PSK.
+        return formatSwarmKey(string(data))
+    }
 
-	return nil, nil
+    // If 'filepath' is empty, return nil for the PSK and no error (indicating no PSK available).
+    return nil, nil
 }
 
+/*It extracts private and public key data from file paths. 
+The private key is read from the specified path, while the public key can be sourced from either a 
+provided public key file or generated from the private key. The parsed key data is returned in a 
+structure that represents domain validation.*/
 func parseValidationKey(key *config.DVKey) (config.DomainValidation, error) {
 	// Private Key
 	privateKey, err := os.ReadFile(key.Private)
@@ -150,6 +194,9 @@ func parseValidationKey(key *config.DVKey) (config.DomainValidation, error) {
 1. Auth needs private key to start properly
 2. Monkey/Substrate either need a public key or a private key to generate a public key from
 */
+/*The validateKeys function serves to validate keys based on the protocols and key data provided. 
+It ensures that specific keys are neither empty nor missing, based on the protocols being utilized. 
+This is crucial for maintaining the integrity and security of network operations.*/
 func validateKeys(protocols []string, privateKey, publicKey string) error {
 	if slices.Contains(protocols, "auth") && privateKey == "" {
 		return errors.New("domains private key cannot be empty when running auth")
