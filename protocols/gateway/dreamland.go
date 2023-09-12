0e4b083e29969336e1c8374fa436f7db567ea785
Tafseer Khan <tafseer.k.khan@gmail.com>
2023-09-12 10:17:21 -0500

    added dreamland gateway calls

--- protocols/gateway/dreamland.go
+++ protocols/gateway/dreamland.go
@@ -1,33 +1,35 @@
 package gateway
 
-// import (
-// 	"context"
-// 	"fmt"
+import (
+	"context"
+	"fmt"
 
-// 	iface "github.com/taubyte/go-interfaces/common"
-// 	tauConfig "github.com/taubyte/tau/config"
-// 	dreamlandCommon "github.com/taubyte/tau/libdream/common"
-// 	dreamlandRegistry "github.com/taubyte/tau/libdream/registry"
-// )
+	iface "github.com/taubyte/go-interfaces/common"
+	commonSpecs "github.com/taubyte/go-specs/common"
+	tauConfig "github.com/taubyte/tau/config"
+	"github.com/taubyte/tau/libdream"
+)
 
-// func init() {
-// 	dreamlandRegistry.Registry.Gateway.Service = createGateway
-// }
+func init() {
+	if err := libdream.Registry.Set(commonSpecs.Gateway, createGateway, nil); err != nil {
+		panic(err)
+	}
+}
 
-// func createGateway(ctx context.Context, config *iface.ServiceConfig) (iface.Service, error) {
-// 	serviceConfig := &tauConfig.Node{
-// 		Ports:       make(map[string]int),
-// 		Root:        config.Root,
-// 		P2PListen:   []string{fmt.Sprintf(dreamlandCommon.DefaultP2PListenFormat, config.Port)},
-// 		P2PAnnounce: []string{fmt.Sprintf(dreamlandCommon.DefaultP2PListenFormat, config.Port)},
-// 		DevMode:     true,
-// 		SwarmKey:    config.SwarmKey,
-// 		HttpListen:  fmt.Sprintf("%s:%d", dreamlandCommon.DefaultHost, config.Others["http"]),
-// 	}
+func createGateway(ctx context.Context, config *iface.ServiceConfig) (iface.Service, error) {
+	serviceConfig := &tauConfig.Node{
+		Ports:       make(map[string]int),
+		Root:        config.Root,
+		P2PListen:   []string{fmt.Sprintf(libdream.DefaultP2PListenFormat, config.Port)},
+		P2PAnnounce: []string{fmt.Sprintf(libdream.DefaultP2PListenFormat, config.Port)},
+		DevMode:     true,
+		SwarmKey:    config.SwarmKey,
+		HttpListen:  fmt.Sprintf("%s:%d", libdream.DefaultHost, config.Others["http"]),
+	}
 
-// 	if result, ok := config.Others["secure"]; ok {
-// 		serviceConfig.EnableHTTPS = result != 0
-// 	}
+	if result, ok := config.Others["secure"]; ok {
+		serviceConfig.EnableHTTPS = result != 0
+	}
 
-// 	return New(ctx, serviceConfig)
-// }
+	return New(ctx, serviceConfig)
+}
--- protocols/gateway/dreamland_test.go
+++ protocols/gateway/dreamland_test.go
@@ -1,116 +1,115 @@
 package gateway
 
-// import (
-// 	"fmt"
-// 	"io"
-// 	"net/http"
-// 	"os"
-// 	"path"
-// 	"testing"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+	"testing"
 
-// 	"github.com/taubyte/config-compiler/decompile"
-// 	commonIface "github.com/taubyte/go-interfaces/common"
-// 	structureSpec "github.com/taubyte/go-specs/structure"
-// 	dreamlandCommon "github.com/taubyte/tau/libdream/common"
-// 	_ "github.com/taubyte/tau/libdream/common/fixtures"
-// 	dreamland "github.com/taubyte/tau/libdream/services"
-// 	_ "github.com/taubyte/tau/protocols/hoarder"
-// 	_ "github.com/taubyte/tau/protocols/monkey"
-// 	"github.com/taubyte/tau/protocols/monkey/fixtures/compile"
-// 	_ "github.com/taubyte/tau/protocols/patrick"
-// 	_ "github.com/taubyte/tau/protocols/seer"
-// 	_ "github.com/taubyte/tau/protocols/substrate"
-// 	_ "github.com/taubyte/tau/protocols/tns"
-// 	"github.com/taubyte/utils/id"
-// 	"gotest.tools/v3/assert"
-// )
+	"github.com/taubyte/config-compiler/decompile"
+	commonIface "github.com/taubyte/go-interfaces/common"
+	structureSpec "github.com/taubyte/go-specs/structure"
+	dreamland "github.com/taubyte/tau/libdream"
+	_ "github.com/taubyte/tau/libdream/fixtures"
+	_ "github.com/taubyte/tau/protocols/hoarder"
+	_ "github.com/taubyte/tau/protocols/monkey"
+	"github.com/taubyte/tau/protocols/monkey/fixtures/compile"
+	_ "github.com/taubyte/tau/protocols/patrick"
+	_ "github.com/taubyte/tau/protocols/seer"
+	_ "github.com/taubyte/tau/protocols/substrate"
+	_ "github.com/taubyte/tau/protocols/tns"
+	"github.com/taubyte/utils/id"
+	"gotest.tools/v3/assert"
+)
 
-// func TestGatewayBasic(t *testing.T) {
-// 	u := dreamland.Multiverse(dreamland.UniverseConfig{Name: t.Name()})
-// 	defer u.Stop()
+func TestGatewayBasic(t *testing.T) {
+	u := dreamland.New(dreamland.UniverseConfig{Name: t.Name()})
+	defer u.Stop()
 
-// 	err := u.StartWithConfig(&dreamlandCommon.Config{
-// 		Services: map[string]commonIface.ServiceConfig{
-// 			"seer":      {},
-// 			"hoarder":   {},
-// 			"tns":       {},
-// 			"substrate": {Others: map[string]int{"copies": 4}},
-// 			"patrick":   {},
-// 			"monkey":    {},
-// 			"gateway":   {},
-// 		},
-// 		Simples: map[string]dreamlandCommon.SimpleConfig{
-// 			"client": {
-// 				Clients: dreamlandCommon.SimpleConfigClients{
-// 					TNS: &commonIface.ClientConfig{},
-// 				},
-// 			},
-// 		},
-// 	})
-// 	assert.NilError(t, err)
+	err := u.StartWithConfig(&dreamland.Config{
+		Services: map[string]commonIface.ServiceConfig{
+			"seer":      {},
+			"hoarder":   {},
+			"tns":       {},
+			"substrate": {Others: map[string]int{"copies": 4}},
+			"patrick":   {},
+			"monkey":    {},
+			"gateway":   {},
+		},
+		Simples: map[string]dreamland.SimpleConfig{
+			"client": {
+				Clients: dreamland.SimpleConfigClients{
+					TNS: &commonIface.ClientConfig{},
+				}.Compat(),
+			},
+		},
+	})
+	assert.NilError(t, err)
 
-// 	projectId := id.Generate()
-// 	functionId := id.Generate()
+	projectId := id.Generate()
+	functionId := id.Generate()
 
-// 	fqdn := "hal.computers.com"
-// 	_path := "ping"
+	fqdn := "hal.computers.com"
+	_path := "ping"
 
-// 	project, err := decompile.MockBuild(projectId, "",
-// 		&structureSpec.Function{
-// 			Id:      functionId,
-// 			Name:    "someFunc",
-// 			Type:    "http",
-// 			Call:    "ping",
-// 			Memory:  100000,
-// 			Timeout: 1000000000,
-// 			Method:  "GET",
-// 			Source:  ".",
-// 			Domains: []string{"someDomain"},
-// 			Paths:   []string{"/" + _path},
-// 		},
-// 		&structureSpec.Domain{
-// 			Name: "someDomain",
-// 			Fqdn: fqdn,
-// 		},
-// 	)
-// 	assert.NilError(t, err)
+	project, err := decompile.MockBuild(projectId, "",
+		&structureSpec.Function{
+			Id:      functionId,
+			Name:    "someFunc",
+			Type:    "http",
+			Call:    "ping",
+			Memory:  100000,
+			Timeout: 1000000000,
+			Method:  "GET",
+			Source:  ".",
+			Domains: []string{"someDomain"},
+			Paths:   []string{"/" + _path},
+		},
+		&structureSpec.Domain{
+			Name: "someDomain",
+			Fqdn: fqdn,
+		},
+	)
+	assert.NilError(t, err)
 
-// 	err = u.RunFixture("injectProject", project)
-// 	assert.NilError(t, err)
+	err = u.RunFixture("injectProject", project)
+	assert.NilError(t, err)
 
-// 	wd, err := os.Getwd()
-// 	assert.NilError(t, err)
+	wd, err := os.Getwd()
+	assert.NilError(t, err)
 
-// 	err = u.RunFixture("compileFor", compile.BasicCompileFor{
-// 		ProjectId:  projectId,
-// 		ResourceId: functionId,
-// 		Paths:      []string{path.Join(wd, "fixtures", "ping.zwasm")},
-// 	})
-// 	assert.NilError(t, err)
+	err = u.RunFixture("compileFor", compile.BasicCompileFor{
+		ProjectId:  projectId,
+		ResourceId: functionId,
+		Paths:      []string{path.Join(wd, "fixtures", "ping.zwasm")},
+	})
+	assert.NilError(t, err)
 
-// 	gateway := u.Gateway()
-// 	gateWayHttpPort, err := u.GetPortHttp(gateway.Node())
-// 	assert.NilError(t, err)
+	gateway := u.Gateway()
+	gateWayHttpPort, err := u.GetPortHttp(gateway.Node())
+	assert.NilError(t, err)
 
-// 	firstSubstrate := u.Substrate()
-// 	substrateHttpPort, err := u.GetPortHttp(firstSubstrate.Node())
-// 	assert.NilError(t, err)
+	firstSubstrate := u.Substrate()
+	substrateHttpPort, err := u.GetPortHttp(firstSubstrate.Node())
+	assert.NilError(t, err)
 
-// 	url := fmt.Sprintf("http://%s:%d/%s", fqdn, substrateHttpPort, _path)
-// 	res, err := http.DefaultClient.Get(url)
-// 	assert.NilError(t, err)
+	url := fmt.Sprintf("http://%s:%d/%s", fqdn, substrateHttpPort, _path)
+	res, err := http.DefaultClient.Get(url)
+	assert.NilError(t, err)
 
-// 	data, err := io.ReadAll(res.Body)
-// 	assert.NilError(t, err)
-// 	assert.Equal(t, "PONG", string(data))
+	data, err := io.ReadAll(res.Body)
+	assert.NilError(t, err)
+	assert.Equal(t, "PONG", string(data))
 
-// 	url = fmt.Sprintf("http://%s:%d/%s", fqdn, gateWayHttpPort, _path)
-// 	res, err = http.DefaultClient.Get(url)
-// 	assert.NilError(t, err)
+	url = fmt.Sprintf("http://%s:%d/%s", fqdn, gateWayHttpPort, _path)
+	res, err = http.DefaultClient.Get(url)
+	assert.NilError(t, err)
 
-// 	data, err = io.ReadAll(res.Body)
-// 	assert.NilError(t, err)
+	data, err = io.ReadAll(res.Body)
+	assert.NilError(t, err)
 
-// 	assert.Equal(t, res.StatusCode, 200, "Gateway Response:", string(data))
-// 	assert.Equal(t, firstSubstrate.Node().ID().String(), string(data))
-// }
+	assert.Equal(t, res.StatusCode, 200, "Gateway Response:", string(data))
+	assert.Equal(t, firstSubstrate.Node().ID().String(), string(data))
+}
