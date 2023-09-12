04932c888b8cad6501b0bbd2d8ab21d9ee38e9ce
nrhill1 <nrhill1@gmail.com>
2023-09-12 09:43:17 -0700

    Adding in comments to clients/http/new.go

--- .history/clients/http/new_20230912093251.go
+++ .history/clients/http/new_20230912093251.go
@@ -7,39 +7,38 @@ import (
 	"net/http"
 )
 
-// This function is used to create a new HTTP client or returns an error.
 func New(ctx context.Context, options ...Option) (*Client, error) {
 	c := &Client{
 		timeout:  DefaultTimeout,
 		ctx:      ctx,
 		provider: string(Github),
 	}
-	// For each of the options, apply them to the client or return an error.
+
 	for _, opt := range options {
 		if err := opt(c); err != nil {
 			return nil, fmt.Errorf("client options failed with: %w", err)
 		}
 	}
-	// Create a new HTTP client and specify the timeout.
+
 	c.client = &http.Client{
 		Timeout: c.timeout,
 	}
-	// If the client is unsecure, create a new HTTP client without TLS verification.
+
 	if c.unsecure {
 		c.client.Transport = &http.Transport{
 			TLSClientConfig: &tls.Config{
 				InsecureSkipVerify: true,
 			},
 		}
-	} else { // If the client is secure, create a new HTTP client with TLS and specify the root CA.
+	} else {
 		c.client.Transport = &http.Transport{
 			TLSClientConfig: &tls.Config{
 				RootCAs: rootCAs,
 			},
 		}
 	}
-	// Set the authorization header for the HTTP client.
+
 	c.auth_header = fmt.Sprintf("%s %s", c.provider, c.token)
-	// Return the client or nothing.
+
 	return c, nil
 }
