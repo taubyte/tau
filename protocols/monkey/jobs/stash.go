e12f1f40eea3352b3fd09622f45db4609212393e
Tafseer Khan <tafseer.k.khan@gmail.com>
2023-09-15 16:59:52 -0500

    handle retry if err

--- protocols/monkey/jobs/stash.go
+++ protocols/monkey/jobs/stash.go
@@ -14,29 +14,30 @@ func (c Context) StashBuildFile(zip io.ReadSeekCloser) (cid string, err error) {
 		return
 	}
 
-	_, err = c.Monkey.Hoarder().Stash(cid)
+	if _, err = c.Monkey.Hoarder().Stash(cid); err != nil {
+		err = nil
+		go func() {
+			ctx, ctxC := context.WithTimeout(c.ctx, 10*time.Minute)
+			defer ctxC()
 
-	go func() {
-		ctx, ctxC := context.WithTimeout(c.ctx, 10*time.Minute)
-		defer ctxC()
-
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case <-time.After(10 * time.Second):
-
-				_, err = c.Monkey.Hoarder().Stash(cid)
-				if err != nil {
-					logger.Errorf("stashing `%s` failed with: %s", cid, err.Error())
-					continue
-				} else {
-					logger.Infof("stashing `%s` suceeded", cid)
+			for {
+				select {
+				case <-ctx.Done():
 					return
+				case <-time.After(10 * time.Second):
+
+					_, err = c.Monkey.Hoarder().Stash(cid)
+					if err != nil {
+						logger.Errorf("stashing `%s` failed with: %s", cid, err.Error())
+						continue
+					} else {
+						logger.Infof("stashing `%s` suceeded", cid)
+						return
+					}
 				}
 			}
-		}
-	}()
+		}()
+	}
 
 	return
 }
