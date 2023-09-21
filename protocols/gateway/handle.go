f114cf61d4ab2861c5509487434331f2f2395780
Tafseer Khan <tafseer.k.khan@gmail.com>
2023-09-21 10:03:41 -0500

    fixed substrate client defaults, close all responses from substrate nodes

--- protocols/gateway/handle.go
+++ protocols/gateway/handle.go
@@ -29,26 +29,52 @@ func (g *Gateway) handleHttp(w goHttp.ResponseWriter, r *goHttp.Request) error {
 		return fmt.Errorf("substrate client proxyHttp failed with: %w", err)
 	}
 
-	highScore := -1
 	var match *client.Response
-	for highScore < MaxScore {
-		response, ok := <-resCh
-		if !ok {
-			break
+	doneChan := make(chan struct{}, 1)
+	var clearMode bool
+	go func() {
+		for response := range resCh {
+			if clearMode {
+				response.Close()
+				continue
+			}
+
+			if err := response.Error(); err != nil {
+				logger.Errorf("peer `%s` response for proxy failed with: %w", response.PID().Pretty(), err)
+				continue
+			}
+
+			var skipClose bool
+			if match == nil {
+				match = response
+				skipClose = true
+			}
+
+			if g.Get(response).Cached() {
+				if !skipClose {
+					match.Close()
+				}
+
+				match = response
+				clearMode = true
+				doneChan <- struct{}{}
+			}
+
 		}
-		if g.Get(response).Cached() {
-			highScore = MaxScore
-			match = response
-		}
-	}
+
+		close(doneChan)
+	}()
+	<-doneChan
+
 	if match == nil {
 		return errors.New("no substrate match found")
 	}
+	defer match.Close()
 
 	w.Header().Add(ProxyHeader, match.PID().Pretty())
 
 	if err := tunnel.Frontend(w, r, match); err != nil {
-		return err
+		return fmt.Errorf("tunneling Frontend failed with: %w", err)
 	}
 
 	return nil
