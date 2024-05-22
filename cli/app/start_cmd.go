422002ae643e6e7728c7949e0fa87101fc33af7c
Andrei Gonchar <jeron312@gmail.com>
2024-05-22 21:01:01 +0300

    Delete old migration code

--- cli/app/start_cmd.go
+++ cli/app/start_cmd.go
@@ -2,8 +2,6 @@ package app
 
 import (
 	"fmt"
-	"os"
-	"os/exec"
 
 	"github.com/taubyte/tau/cli/node"
 	"github.com/taubyte/tau/config"
@@ -36,22 +34,6 @@ func startCommand() *cli.Command {
 			if err != nil {
 				return fmt.Errorf("parsing config failed with: %s", err)
 			}
-
-			// Migration Start (from odo to tau)
-			// TODO: Delete this after a few releases
-			if _, err := os.Stat(fmt.Sprintf("/tb/storage/databases/%s", shape)); !os.IsNotExist(err) {
-				err = migrateDatabase(ctx.Context, shape, len(protocolConfig.Protocols) == 0)
-				if err != nil {
-					return fmt.Errorf("migrating shape %s failed with: %w", shape, err)
-				}
-
-				cmd := exec.Command("sudo", "systemctl", "stop", fmt.Sprintf("odo@%s.service", shape))
-				cmd.CombinedOutput()
-
-				cmd = exec.Command("sudo", "systemctl", "disable", fmt.Sprintf("odo@%s.service", shape))
-				cmd.CombinedOutput()
-			}
-
 			return node.Start(ctx.Context, protocolConfig)
 		},
 	}
