ab74b9d3e494848b14cbf3af1e69b27e60b5fb28
Andrei Gonchar <jeron312@gmail.com>
2024-05-23 17:04:18 +0300

    Add select wrapper (#158)
    Better handling of job delay
    ---------
    Co-authored-by: samyfodil <fodil.samy@gmail.com>

--- protocols/monkey/jobs/run.go
+++ protocols/monkey/jobs/run.go
@@ -2,13 +2,15 @@ package jobs
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"github.com/ipfs/go-log/v2"
 )
 
 var (
-	logger = log.Logger("tau.monkey.jobs.client")
+	logger               = log.Logger("tau.monkey.jobs.client")
+	ErrorContextCanceled = errors.New("context cancel")
 )
 
 func (c *Context) Run(ctx context.Context, ctxC context.CancelFunc) (err error) {
@@ -17,7 +19,11 @@ func (c *Context) Run(ctx context.Context, ctxC context.CancelFunc) (err error) 
 	defer ctxC()
 
 	if c.Job.Delay != nil {
-		<-time.After(time.Duration(c.Job.Delay.Time) * time.Second)
+		select {
+		case <-time.After(time.Duration(c.Job.Delay.Time) * time.Second):
+		case <-c.ctx.Done():
+			return ErrorContextCanceled
+		}
 	}
 
 	if err = c.cloneAndSet(); err != nil {
