a34a75d34c477b112d20335d0de4734974f36e24
samyfodil <fodil.samy@gmail.com>
2025-08-12 12:03:03 -0500

    spore-drive python sdk version 0.1.1

--- pkg/spore-drive/clients/py/setup.py
+++ pkg/spore-drive/clients/py/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="spore-drive",
-    version="0.1.0",
+    version="0.1.1",
     author="Taubyte",
     author_email="dev@taubyte.com",
     description="Python client for Spore Drive",
